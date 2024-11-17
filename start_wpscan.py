ad3beb5f2afadef95aa7e1916d8f188b72cadc8b
EXxX0RCIST <b865910163@gmail.com>
2024-11-17 14:20:10 +0300

    Update start_wpscan.py

--- start_wpscan.py
+++ start_wpscan.py
@@ -3,12 +3,14 @@ import subprocess
 class wpscan:
     def __init__(self) -> None:
         self.targets = open('targets.txt', 'r')
-        self.TRIGER = ""
+        self.TRIGER = set()
+        self.SAVE_OUTPUT = False
         self.scanout = open('wpscan_out', 'a')
 
     def start_scan(self, target):
         
-        param = ['wpscan', '--url', f'{target}', '--enumerate', 'p']
+        param = ['wpscan', '--url', f'{target}', '--enumerate', 'p', 
+                 '--ignore-main-redirect', '--rua']
 
         process = subprocess.Popen(param, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
@@ -19,20 +21,27 @@ class wpscan:
 
         if process.returncode == 0:
             if self.TRIGER in output:
-                with open('sqli.txt', 'a') as sql_save:
-                    sql_save.write(f"{param}\n")
+                self.scanout.write(f'SCAN SITE: {target}\nDETECTED TRIGER\n{output}\n\n')
                 print(f"Scan {param[2]} activate triger {self.TRIGER}")
             else:
-                print(f"Site {param[2].strip('"')} not have critical vulnerability((")            
+                print(f"Site {param[2]} not have critical vulnerability((")
+                if self.SAVE_OUTPUT:
+                    self.scanout.write(f'SCAN SITE: {target}\n{output}\n\n')
+
         else:
             print(f"ERROR: \n{error_output}")
             return
-        self.scanout.write(f'SCAN SITE: {target}\n{output}\n\n\n')
         print(output)
 
     def set_triger(self, tr):
-        self.TRIGER = tr
-        print(f'Triger="{tr}"')
+        self.TRIGER = tr.split(', ')
+        print(f'Triger=', end='')
+        for x in self.TRIGER:
+            print(x)
+    
+    def set_save_output(self):
+        self.SAVE_OUTPUT = True
+        print("Save output=True")
 
     def main_scan(self):
         for target in self.targets:
